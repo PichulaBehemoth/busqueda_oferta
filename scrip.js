052e7b92b5ad01867d791ce53b0dd2fdd32a0f7c
Vasoga <129012819+Vasoga@users.noreply.github.com>
2023-05-16 13:50:58 -0600

    Update scrip.js

--- scrip.js
+++ scrip.js
@@ -35,9 +35,10 @@ function displayResults(results) {
 function searchAliExpress(query) {
   // Ejemplo de búsqueda ficticia en AliExpress con resultados de imágenes
   const results = [
-    { image: 'https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSmHms60UJ2kod1SvGvcbIYb2YClRab-K023kYFgOgpAbcGepY3N6MnFc-sf7QteXxrv4Os4y0FDr2PJC2SCKUD1qb5plEbYnDS2luatySjjzxu9Xgiuvd4QQMznJFjZiYkGQ&usqp=CAc',  'laptop', price: '$4,199' },
-    { image: 'ruta_imagen_2.jpg', title: 'Producto 2', price: '$15' },
-    { image: 'ruta_imagen_3.jpg', title: 'Producto 3', price: '$20' },
+    
+    { image: 'https://m.media-amazon.com/images/I/61zaZSxX+SL._AC_SY355_.jpg',  title: 'Shampoo', price:'$158,00' },
+    { image: 'https://m.media-amazon.com/images/I/617w2tb+YeL._AC_SX425_.jpg', title: 'Acondicionador', price: '$52,50' },
+    { image: 'https://d1zc67o3u1epb0.cloudfront.net/media/catalog/product/cache/23527bda4807566b561286b47d9060f4/1/6/16434_1.jpg', title: 'Shampoo', price: '$80.70' },
     // ...
   ];
 
